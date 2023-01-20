35b7387b076082425106e8a62ca696835fd5c982
Hunter Czarapata <czarapata.hunter@gmail.com>
2023-01-19 18:42:45 -0800

    CSS Styling for Health, Attack, and Defense

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -7,11 +7,13 @@ import Health from '../../../src/heartIcon.png';
 export default function PokemonCard({ pokemon, hp, attack, defense, url_image, type_1, type_2 }) {
   return (
     <div className="PokemonCard">
-      <p className="pokemon">{pokemon}</p>
-      <img src={url_image} style={{ height: '100px', width: 'auto' }} />
+      <div>
+        <p className="pokemon">{pokemon}</p>
+        <img src={url_image} style={{ height: '100px', width: 'auto' }} />
+      </div>
       <span className="health">
-        <img src={Health} style={{ height: '20px', width: '20px' }} />
-        <p className="hp">{hp}</p>
+        <img src={Health} style={{ height: '17px', width: '17px' }} />
+        <p className="health">{hp}</p>
       </span>
       <div className="fighting">
         <span className="attack">
